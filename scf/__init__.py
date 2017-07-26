2b87b465b0ef64f208be0e02f277410d013553e8
hongzhouye <hzye2011@mit.edu>
2017-07-26 16:44:37 -0400

    Cleans tests directory

--- scf/__init__.py
+++ scf/__init__.py
@@ -6,4 +6,3 @@ Initializes everything for the SCF program.
 # import the HF driver
 from . import hf
 from .hf import *
-from . import math
--- tests/test_math.py
+++
@@ -1,6 +0,0 @@
-
-import scf
-
-
-def test_add():
-    assert scf.math.add(5, 2) == 7
