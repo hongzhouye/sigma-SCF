145d03d014a19764661efcd060bb83561219946b
Nadav <gevahn@mit.edu>
2017-07-27 10:11:09 -0400

    added input file foramts, initilizer

--- scf/init.py
+++ scf/init.py
@@ -3,9 +3,37 @@ Initializations for scf calculations:
     - preparing integrals
     - parsing & packing parameters
 """
+import yaml
+import psi4wrapper as p4w
+
+scf_params = None
+ao_ints = None
 
 
-def init():
+def parse_yaml(file_name):
     """
+    parses a yaml file and returns a python dict
     """
-    pass
+    dict_to_return = None
+    with open(file_name, 'r') as yaml_file:
+        dict_to_return = yaml.load(yaml_file)
+    return dict_to_return
+
+
+def init(input_file):
+    """
+    setup the parameters for thre job
+    """
+    global scf_params
+    global ao_ints
+    defualts_location = "scf_params_default.yml"
+    scf_params = parse_yaml(defualts_location)
+    user_input = parse_yaml(input_file)
+    for key in user_input.keys():
+        if key in scf_params.keys():
+            scf_params[key] = user_input[key]
+#    ao_ints = p4w.init(scf_params)
+
+if __name__ == "__main__":
+    init("test.yml")
+    print(scf_params)
