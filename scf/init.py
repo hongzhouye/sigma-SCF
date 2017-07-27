73987025311e15fea36e56276cc20316e6add1b0
Nadav <gevahn@mit.edu>
2017-07-27 13:42:51 -0400

    fixed typo

--- scf/init.py
+++ scf/init.py
@@ -8,7 +8,7 @@ import psi4wrapper as p4w
 
 scf_params = None
 ao_ints = None
-
+e_ZZ_repul = None
 
 def parse_yaml(file_name):
     """
@@ -26,13 +26,17 @@ def init(input_file):
     """
     global scf_params
     global ao_ints
-    defualts_location = "scf_params_default.yml"
-    scf_params = parse_yaml(defualts_location)
+    global e_ZZ_repul
+
+    defaults_location = "scf_params_default.yml"
+    scf_params = parse_yaml(defaults_location)
     user_input = parse_yaml(input_file)
     for key in user_input.keys():
         if key in scf_params.keys():
             scf_params[key] = user_input[key]
-#    ao_ints = p4w.init(scf_params)
+    ao_ints, e_ZZ_repul, n_basis, n_el = p4w.init(scf_params)
+    scf_params['nel'] = n_el - scf_params['charge']
+    scf_params['nbas'] = n_basis
 
 if __name__ == "__main__":
     init("test.yml")
