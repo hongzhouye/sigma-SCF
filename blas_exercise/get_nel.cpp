6ae71a1237374ad43eb5794de9a1fcd8514dcbd7
Nadav <gevahn@mit.edu>
2017-07-28 12:00:52 -0400

    fixed case sensitivity

--- blas_exercise/get_nel.cpp
+++ blas_exercise/get_nel.cpp
@@ -1,6 +1,6 @@
 #include <include/get_nel.hpp>
 int get_nel(const std::vector<double>& D, const std::vector<double>& S) {
     int n = D.size();
-    double nel = lawrap::dot(n, D.data(), 1, S.data(), 1);
+    double nel = LAWrap::dot(n, D.data(), 1, S.data(), 1);
     return (int)std::round(nel);
 }
