4560528bf4afc06f19d6306b170f1cd18debd78b
Shannon Houck <shannonhouck@shannon-book.local>
2017-07-28 17:40:47 -0400

    I think DIIS works now!!

--- scf/scf_utils.py
+++ scf/scf_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from diis_solver import diis_solver
+
 
 def get_dm(C, nel):
     D = C[:, :nel]
@@ -6,10 +8,31 @@ def get_dm(C, nel):
     return D
 
 
-def get_fock(H, g, D):
-    J = np.einsum("pqrs,rs->pq", g, D)
-    K = np.einsum("prqs,rs->pq", g, D)
-    return H + 2 * J - K
+def get_JK(is_fitted, g, D):
+    if(is_fitted):
+        # FINISH LATER
+        J = np.einsum("pqrs,rs->pq", g, D)
+        K = np.einsum("prqs,rs->pq", g, D)
+        return (J, K)
+    else:
+        J = np.einsum("pqrs,rs->pq", g, D)
+        K = np.einsum("prqs,rs->pq", g, D)
+        return (J, K)
+
+
+def get_fock(H, g, D, opt, F_prev_list, r_prev_list):
+    opt = opt.upper()
+    # not accelerated
+    if(opt == 'NONE' or len(F_prev_list) == 0):
+        J, K = get_JK(False, g, D)
+        return H + 2 * J - K
+    # DIIS
+    elif(opt == 'DIIS'):
+        c = diis_solver(r_prev_list) # GET THE COEFFICIENTS!!
+        out = 0 * H
+        for i, element in enumerate(F_prev_list):
+            out += c[i] * element
+        return out
 
 
 def diag(F, A):
@@ -20,4 +43,4 @@ def diag(F, A):
 
 
 def get_SCF_err(S, D, F):
-    return np.sum(np.abs(S @ D @ F - F @ D @ S))
+    return (np.sum(np.abs(S @ D @ F - F @ D @ S)), (S @ D @ F - F @ D @ S))
