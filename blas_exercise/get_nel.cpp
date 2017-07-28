284bfe0ed0a21e5df76443c14bca21084d806b17
Nadav <gevahn@mit.edu>
2017-07-28 11:55:25 -0400

    get number of electrons

---
+++ blas_exercise/get_nel.cpp
@@ -0,0 +1,6 @@
+
+int get_nel(const std::vector<double>& D, const std::vector<double>& S) {
+    int n = D.size();
+    double nel = lawrap::dot(n, D.data(), 1, S.data(), 1);
+    return (int)std::round(nel);
+}
---
+++ blas_exercise/get_nel.hpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <lawrap/blas.h>
+
+int get_nel(const std::vector<double>& D, const std::vector<double>& S); 
