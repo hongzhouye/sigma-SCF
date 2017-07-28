7b92d1407e9572525f1fcc874440ed4c2323e1b1
Nadav <gevahn@mit.edu>
2017-07-28 11:57:39 -0400

    oved header

--- blas_exercise/get_nel.cpp
+++ blas_exercise/get_nel.cpp
@@ -1,4 +1,4 @@
-
+#include <include/get_nel.hpp>
 int get_nel(const std::vector<double>& D, const std::vector<double>& S) {
     int n = D.size();
     double nel = lawrap::dot(n, D.data(), 1, S.data(), 1);
--- blas_exercise/get_nel.hpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include <lawrap/blas.h>
-
-int get_nel(const std::vector<double>& D, const std::vector<double>& S); 
