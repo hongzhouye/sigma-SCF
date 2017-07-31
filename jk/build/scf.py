f4952e135abcb0aaa2cdd1e7b7c06549e344d6bb
hongzhouye <hzye2011@mit.edu>
2017-07-31 19:19:34 -0400

    Implemented getJK_np_Dshift

--- jk/build/scf.py
+++ jk/build/scf.py
@@ -1,6 +1,6 @@
 import numpy as np
-import psi4
 import jk
+import psi4
 import time
 
 # Make sure we get the same random array
@@ -14,42 +14,34 @@ H 1 1.1 2 104
 """)
 
 # Build a ERI tensor
-basis = psi4.core.BasisSet.build(mol, target="cc-pvdz")
+basis = psi4.core.BasisSet.build(mol, target="cc-pvqz")
 mints = psi4.core.MintsHelper(basis)
 I = np.array(mints.ao_eri())
+print("nbas = ", I.shape[0])
 
 # Symmetric random density
 nbf = I.shape[0]
 D = np.random.rand(nbf, nbf)
 D = (D + D.T) / 2
 
+n_trials = 1
+
 # Reference
 start = time.time()
-for i in range(1000):
+for i in range(n_trials):
     J_ref = np.einsum("pqrs,rs->pq", I, D)
     K_ref = np.einsum("prqs,rs->pq", I, D)
 end = time.time()
 print(end - start)
 
 # Your implementation
-start = time.time()
 II = np.swapaxes(I, 1, 2).copy()
-for i in range(1000):
-    J = jk.getJK_np(I, D)
-    K = jk.getJK_np(II, D)
-end = time.time()
-print(end - start)
-
-# Your implementation 2
 start = time.time()
-for i in range(1000):
-    J2 = jk.getJ_np(I, D)
-    K2 = jk.getK_np(II, D)
+for i in range(n_trials):
+    J, K = jk.getJK_np(I, I.swapaxes(1, 2).copy(), D)
 end = time.time()
 print(end - start)
 
 # Make sure your implementation is correct
 print("J is correct: %s" % np.allclose(J, J_ref))
 print("K is correct: %s" % np.allclose(K, K_ref))
-print("J2 is correct: %s" % np.allclose(J2, J_ref))
-print("K2 is correct: %s" % np.allclose(K2, K_ref))
