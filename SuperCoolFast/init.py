8a39e6cd3711326b1cf36b96e3261531a25837d0
hongzhouye <hzye2011@mit.edu>
2017-08-02 08:15:14 -0400

    Adds keyword 'spin' in params

--- SuperCoolFast/init.py
+++ SuperCoolFast/init.py
@@ -35,12 +35,14 @@ def init(input_file):
     for key in user_input.keys():
         if key in scf_params.keys():
             scf_params[key] = user_input[key]
-    ao_ints, e_ZZ_repul, n_el, n_basis = p4w.init(scf_params)
-    scf_params['nel'] = n_el - scf_params['charge']
-    if (scf_params['nel'] % 2 == 1):
-        raise Exception("only closed shell molecules are supported!")
+    ao_ints, e_ZZ_repul, n_el_tot, n_basis = p4w.init(scf_params)
+    n_el_tot -= scf_params['charge']    # total num of electrons
+    n_el_diff = scf_params['spin'] - 1  # n_el_alpha - n_el_beta
+    if (n_el_tot + n_el_diff) % 2 == 1:
+        raise Exception("Charge and spin are not compatible.")
     else:
-        scf_params['nel'] = int(scf_params['nel'] / 2)
+        scf_params['nel_alpha'] = int((n_el_tot + n_el_diff) / 2)
+        scf_params['nel_beta'] = int((n_el_tot - n_el_diff) / 2)
     scf_params['nbas'] = n_basis
     print(scf_params)
     return ao_ints, scf_params, e_ZZ_repul
