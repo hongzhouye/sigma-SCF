d0982f7e7d30a9a7ee9a3eca38100873a79b16eb
hongzhouye <hzye2011@mit.edu>
2017-08-04 15:35:10 -0400

    Modifies energy evaluation in SuperCoolFast.py

--- SuperCoolFast/SuperCoolFast.py
+++ SuperCoolFast/SuperCoolFast.py
@@ -7,8 +7,8 @@ import numpy
 if __name__ == "__main__":
     ao_ints, scf_params, e_ZZ_repul = init(sys.argv[1])
     eps, C, D, F = scf(ao_ints, scf_params)
-    H = ao_ints['T'] + ao_ints['V']
-    energy = np.sum((F + H) * D) + e_ZZ_repul
+    energy = get_SCF_energy(ao_ints, F, D, scf_params['unrestricted']) \
+        + e_ZZ_repul
     print("\nFINAL SCF ENERGY: {}\n".format(energy))
 
     # mp2
