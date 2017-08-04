a4a796a83a1be7f7983c2d869c2647a11a650e4d
hongzhouye <hzye2011@mit.edu>
2017-08-04 15:35:35 -0400

    Adds an uhf diis_solver in diis_solver.py

--- scf/diis_solver.py
+++ scf/diis_solver.py
@@ -21,7 +21,34 @@ def diis_solver(errs):
     b = np.zeros(n + 1)
     b[n] = -1
 
-    x = np.linalg.solve(A, b)
+    x = np.linalg.pinv(A) @ b
+
+    c = x[:n]
+
+    return c
+
+
+def diis_solver_uhf(errs, errbs):
+    n = len(errs)
+
+    B = np.zeros([n, n])
+    for i, erra in enumerate(errs):
+        for j, errb in enumerate(errs):
+            B[i, j] = erra.ravel() @ errb.ravel()
+
+    for i, erra in enumerate(errbs):
+        for j, errb in enumerate(errbs):
+            B[i, j] += erra.ravel() @ errb.ravel()
+
+    A = np.ones([n + 1, n + 1])
+    A = -A
+    A[:n, :n] = B
+    A[n, n] = 0
+
+    b = np.zeros(n + 1)
+    b[n] = -1
+
+    x = np.linalg.pinv(A) @ b
 
     c = x[:n]
 
