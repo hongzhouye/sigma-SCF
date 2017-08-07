c62fc6ffdcdc8f3909daefe4bc3e9a75b15f8ff0
hongzhouye <hzye2011@mit.edu>
2017-08-07 16:05:41 -0400

    Adds more tests for sscf

--- tests/test_sscf.py
+++ tests/test_sscf.py
@@ -10,16 +10,31 @@ def test_sscf():
     # get integrals and scf parameters
     ao_ints, test_scf_param, e_nuc = scf.init(\
         os.path.dirname(__file__) + "/test_sscf.yml")
-    # Spin-restricted sigma-SCF
-    eps, C, D, F = scf.rsscf(ao_ints, test_scf_param, e_nuc)
-    e_rsscf = scf.get_SCF_energy(ao_ints, F, D, False) + e_nuc
-    # Spin-unrestricted sigma-SCF (not break spin symmetry)
-    epss, Cs, Ds, Fs = scf.usscf(ao_ints, test_scf_param, e_nuc)
-    e_usscf = scf.get_SCF_energy(ao_ints, Fs, Ds, True) + e_nuc
-    # Spin-unrestricted sigma-SCF (break spin symmetry)
+    # Spin-restricted sigma-SCF, DIIS
+    test_scf_param['unrestricted'] = False
+    eps, C, D, F = scf.sscf(ao_ints, test_scf_param, e_nuc)
+    e_rsscf_diis = scf.get_SCF_energy(ao_ints, F, D, False) + e_nuc
+    # Spin-restricted sigma-SCF, ODA
+    test_scf_param['opt'] = "oda"
+    eps, C, D, F = scf.sscf(ao_ints, test_scf_param, e_nuc)
+    e_rsscf_oda = scf.get_SCF_energy(ao_ints, F, D, False) + e_nuc
+    # Spin-unrestricted sigma-SCF (not break spin symmetry), DIIS
+    test_scf_param['unrestricted'] = True
+    test_scf_param['opt'] = "diis"
+    epss, Cs, Ds, Fs = scf.sscf(ao_ints, test_scf_param, e_nuc)
+    e_usscf_diis = scf.get_SCF_energy(ao_ints, Fs, Ds, True) + e_nuc
+    # Spin-unrestricted sigma-SCF (not break spin symmetry), ODA
+    test_scf_param['unrestricted'] = True
+    test_scf_param['opt'] = "oda"
+    epss, Cs, Ds, Fs = scf.sscf(ao_ints, test_scf_param, e_nuc)
+    e_usscf_oda = scf.get_SCF_energy(ao_ints, Fs, Ds, True) + e_nuc
+    # Spin-unrestricted sigma-SCF (break spin symmetry), DIIS
     test_scf_param['homo_lumo_mix'] = 3
-    epss, Cs, Ds, Fs = scf.usscf(ao_ints, test_scf_param, e_nuc)
-    e_usscf2 = scf.get_SCF_energy(ao_ints, Fs, Ds, True) + e_nuc
+    test_scf_param['opt'] = "diis"
+    epss, Cs, Ds, Fs = scf.sscf(ao_ints, test_scf_param, e_nuc)
+    e_usscf_diis_bs = scf.get_SCF_energy(ao_ints, Fs, Ds, True) + e_nuc
     # check
-    assert(np.allclose(e_rsscf, e_usscf) == True)
-    assert(e_rsscf > e_usscf2)
+    assert(np.allclose(e_rsscf_diis, e_rsscf_oda) == True)
+    assert(np.allclose(e_rsscf_diis, e_usscf_diis) == True)
+    assert(np.allclose(e_rsscf_diis, e_usscf_oda) == True)
+    assert(e_rsscf_diis > e_usscf_diis_bs)
